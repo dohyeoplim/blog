a139f9e7fe6ddda652c1909887a66e2e44f20867
Dohyeop Lim <dhlim0817@gmail.com>
2025-02-18 22:48:06 +0900

    feat(types): add PostMeta interface

---
+++ src/types/PostMeta.ts
@@ -0,0 +1,7 @@
+export default interface PostMeta {
+    title: string;
+    description?: string;
+    date: string;
+    tags?: string[];
+    slug: string;
+}
--- src/types/post.ts
+++
@@ -1,7 +0,0 @@
-export interface Post {
-    title: string;
-    excerpt?: string;
-    slug: string;
-    date: string;
-    tags?: string[];
-}
