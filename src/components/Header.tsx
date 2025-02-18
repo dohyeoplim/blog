2a800e1f9a633312517d4d65d5083710c8d92c31
Dohyeop Lim <dhlim0817@gmail.com>
2025-02-19 02:11:59 +0900

    feat(header): morphing animations

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,35 @@
+"use client";
+
+import { usePathname } from "next/navigation";
+import { Link } from "next-view-transitions";
+
 interface HeaderProps {
     title?: string;
 }
 
-// TODO - metadata based header title
-const Header: React.FC<HeaderProps> = ({ title = "Blog." }) => {
+const Header: React.FC<HeaderProps> = ({ title = "" }) => {
+    const pathname = usePathname();
+    const isHome = pathname === "/";
+
     return (
         <header className="pt-24 mb-16">
-            <h1 className="text-4xl font-bold">{title}</h1>
+            <h1 className="text-4xl font-bold">
+                {isHome ? (
+                    <span style={{ viewTransitionName: "name" }}>
+                        @dohyeoplim
+                    </span>
+                ) : (
+                    <Link
+                        href="/"
+                        className="text-xl font-semibold text-secondary-foreground hover:underline"
+                    >
+                        <span style={{ viewTransitionName: "name" }}>
+                            @dohyeoplim
+                        </span>
+                    </Link>
+                )}
+            </h1>
+            {!isHome && <h1 className="text-4xl font-bold">{title}</h1>}
         </header>
     );
 };
