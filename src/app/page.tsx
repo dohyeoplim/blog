7e1889d8f461da55c6337a985fe0f645bc4ba45f
Dohyeop Lim <dhlim0817@gmail.com>
2025-02-18 20:24:13 +0900

    feat(page): integrate components into Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,17 @@
 // TODO - Metadata
 
+import PreviewList from "@/components/Blog/PreviewList";
+import PreviewListItem from "@/components/Blog/PreviewListItem";
+import Header from "@/components/Header";
+
 const Home = () => {
-    return <h1>Main</h1>;
+    return (
+        <>
+            <Header title="Home" />
+
+            <PreviewList />
+        </>
+    );
 };
 
 export default Home;
