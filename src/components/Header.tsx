dd66d6f5697426000b77c20cf879c3f7252e3adb
Dohyeop Lim <dhlim0817@gmail.com>
2025-02-18 20:19:07 +0900

    style(comp): update Header component padding and margin for improved layout

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,7 @@ interface HeaderProps {
 // TODO - metadata based header title
 const Header: React.FC<HeaderProps> = ({ title = "Blog." }) => {
     return (
-        <header className="pt-16 mb-8">
+        <header className="pt-24 mb-16">
             <h1 className="text-4xl font-bold">{title}</h1>
         </header>
     );
