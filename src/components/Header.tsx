3dee843fc01d34e0bab46a9228c33110e91ff2af
Dohyeop Lim <dhlim0817@gmail.com>
2025-02-19 02:37:21 +0900

    feat(header, theme): integrate ModeToggle component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,6 +2,7 @@
 
 import { usePathname } from "next/navigation";
 import { Link } from "next-view-transitions";
+import { ModeToggle } from "./ThemeSwitch";
 
 interface HeaderProps {
     title?: string;
@@ -12,24 +13,30 @@ const Header: React.FC<HeaderProps> = ({ title = "" }) => {
     const isHome = pathname === "/";
 
     return (
-        <header className="pt-24 mb-16">
-            <h1 className="text-4xl font-bold">
-                {isHome ? (
-                    <span style={{ viewTransitionName: "name" }}>
-                        @dohyeoplim
-                    </span>
-                ) : (
-                    <Link
-                        href="/"
-                        className="text-xl font-semibold text-secondary-foreground hover:underline"
-                    >
+        <header className="pt-24 mb-16 w-full flex justify-between items-center">
+            <div>
+                <h1 className="text-4xl font-bold">
+                    {isHome ? (
                         <span style={{ viewTransitionName: "name" }}>
                             @dohyeoplim
                         </span>
-                    </Link>
-                )}
-            </h1>
-            {!isHome && <h1 className="text-4xl font-bold">{title}</h1>}
+                    ) : (
+                        <Link
+                            href="/"
+                            className="text-xl font-semibold text-secondary-foreground hover:underline"
+                        >
+                            <span style={{ viewTransitionName: "name" }}>
+                                @dohyeoplim
+                            </span>
+                        </Link>
+                    )}
+                </h1>
+                {!isHome && <h1 className="text-4xl font-bold">{title}</h1>}
+            </div>
+
+            <div style={{ viewTransitionName: "mode" }}>
+                <ModeToggle />
+            </div>
         </header>
     );
 };
