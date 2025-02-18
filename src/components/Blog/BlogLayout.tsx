da32ac6e654c340c1c9a0a5427eb1a946eaaf033
Dohyeop Lim <dhlim0817@gmail.com>
2025-02-19 00:25:20 +0900

    feat(blog): update BlogLayout to use PostMeta type for metadata

--- src/components/Blog/BlogLayout.tsx
+++ src/components/Blog/BlogLayout.tsx
@@ -1,9 +1,10 @@
 import Header from "@/components/Header";
+import PostMeta from "@/types/PostMeta";
 
 const BlogLayout = ({
     children,
     metadata,
-}: Readonly<{ children: React.ReactNode; metadata: any }>) => {
+}: Readonly<{ children: React.ReactNode; metadata: PostMeta }>) => {
     return (
         <div>
             <Header title={metadata?.title} />
