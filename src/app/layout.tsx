e111a03aad9af17c44aa86fc9b35ad3545bf4878
Dohyeop Lim <dhlim0817@gmail.com>
2025-02-19 02:27:01 +0900

    feat(theme): add ThemeProvider and ModeToggle components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { ViewTransitions } from "next-view-transitions";
 import { jetbrainsMono, pretendard } from "@/lib/fonts";
+import { ThemeProvider } from "@/components/ThemeProvider";
 
 export const metadata: Metadata = {
     title: "@dohyeoplim",
@@ -18,10 +19,17 @@ export default function RootLayout({
             <html
                 lang="ko"
                 className={`antialiased min-h-screen ${pretendard.variable} ${jetbrainsMono.variable}`}
+                suppressHydrationWarning
             >
                 <body className={`max-w-2xl mx-auto ${pretendard.className}`}>
-                    <main>{children}</main>
-                    {/* Footer */}
+                    <ThemeProvider
+                        attribute="class"
+                        defaultTheme="system"
+                        enableSystem
+                    >
+                        <main>{children}</main>
+                        {/* Footer */}
+                    </ThemeProvider>
                 </body>
             </html>
         </ViewTransitions>
