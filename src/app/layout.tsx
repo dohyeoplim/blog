a4b7a50e88719d88cfe01b7e85f4d60519eef58c
Dohyeop Lim <dhlim0817@gmail.com>
2025-02-20 00:15:13 +0900

    feat: add favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,30 @@ import Footer from "@/components/Footer";
 export const metadata: Metadata = {
     title: "dohyeoplim/blog",
     description: "dohyeoplim's blog",
+    icons: [
+        {
+            rel: "icon",
+            type: "image/x-icon",
+            url: "/static/favicon.ico",
+        },
+        {
+            rel: "apple-touch-icon",
+            sizes: "180x180",
+            url: "/static/icon/apple-touch-icon.png",
+        },
+        {
+            rel: "icon",
+            type: "image/png",
+            sizes: "32x32",
+            url: "/static/icon/favicon-32x32.png",
+        },
+        {
+            rel: "icon",
+            type: "image/png",
+            sizes: "16x16",
+            url: "/static/icon/favicon-16x16.png",
+        },
+    ],
 };
 
 export default function RootLayout({
