e3dc592f646a734751b360840cb1b361dee2b787
Dohyeop Lim <dhlim0817@gmail.com>
2025-02-19 19:55:05 +0900

    style(CodeBlock): make the codeblock scrollable(horizontally)

--- src/components/Blog/CodeBlock.tsx
+++ src/components/Blog/CodeBlock.tsx
@@ -29,26 +29,27 @@ const CodeBlock = ({
     };
 
     return (
-        <div className="not-prose relative bg-zinc-900 text-white rounded-lg p-4 overflow-x-auto my-6">
-            <span className="absolute top-4 left-4 text-xs text-gray-400 uppercase">
+        <div className="not-prose relative bg-zinc-900 text-white rounded-lg p-8">
+            <span className="absolute top-4 left-4 text-xs text-gray-400 uppercase pointer-events-none">
                 {language}
             </span>
 
             <Button
                 onClick={handleCopy}
-                size="sm"
-                className="absolute top-3 right-4 text-xs opacity-80 hover:opacity-100 bg-zinc-700 hover:bg-zinc-600 text-white rounded"
+                className="absolute top-4 right-4 bg-zinc-700 hover:bg-zinc-600 text-white rounded"
             >
                 {copied ? "Copied!" : "Copy"}
             </Button>
 
-            <pre>
-                <code
-                    className="block whitespace-pre-wrap font-mono mt-8"
-                    dangerouslySetInnerHTML={{ __html: highlightedCode }}
-                    {...props}
-                />
-            </pre>
+            <div className="overflow-x-auto">
+                <pre className="p-0 m-0 bg-transparent">
+                    <code
+                        className="block whitespace-pre font-mono"
+                        dangerouslySetInnerHTML={{ __html: highlightedCode }}
+                        {...props}
+                    />
+                </pre>
+            </div>
         </div>
     );
 };
