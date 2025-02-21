b258dbe42628af5fb40c2134e248bec15bd869a0
Dohyeop Lim <dhlim0817@gmail.com>
2025-02-22 04:26:16 +0900

    feat(config): add S3 and Google User Content domains to Next.js config

--- next.config.ts
+++ next.config.ts
@@ -30,6 +30,24 @@ const nextConfig: NextConfig = {
                 port: "",
                 pathname: "/**",
             },
+            {
+                protocol: "https",
+                hostname: "prod-files-secure.s3.us-west-2.amazonaws.com",
+                port: "",
+                pathname: "/**",
+            },
+            {
+                protocol: "https",
+                hostname: "s3.us-west-2.amazonaws.com",
+                port: "",
+                pathname: "/**",
+            },
+            {
+                protocol: "https",
+                hostname: "lh3.googleusercontent.com",
+                port: "",
+                pathname: "/**",
+            },
         ],
     },
 };
