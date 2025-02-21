7232ceea75b47034523243646ff388b829502997
Dohyeop Lim <dhlim0817@gmail.com>
2025-02-22 04:11:28 +0900

    feat(Header): enhance notion rendering page style

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -17,7 +17,58 @@ const Header: React.FC<HeaderProps> = ({
 }) => {
     const pathname = usePathname();
     const isHome = pathname === "/";
-    const MainTitle = "dohyeoplim/blog";
+    const isN = pathname === "/n";
+    const isInN = pathname.startsWith("/n/");
+
+    const renderSubTitle = () => {
+        if (isHome) {
+            return (
+                <span style={{ viewTransitionName: "dohyeoplim" }}>
+                    dohyeoplim/blog
+                </span>
+            );
+        }
+
+        return (
+            <div className="flex items-center space-x-3 text-sm sm:text-base font-semibold text-secondary-foreground">
+                <div className="flex items-center space-x-1">
+                    <Link href="/">
+                        <ArrowLeft className="size-3 sm:size-4" />
+                    </Link>
+                    <Link href="/" className="hover:underline">
+                        <span style={{ viewTransitionName: "dohyeoplim" }}>
+                            dohyeoplim/blog
+                        </span>
+                    </Link>
+                </div>
+                {isInN && (
+                    <div className="flex items-center space-x-3">
+                        <span>/</span>
+                        <Link href="/n" className="hover:underline">
+                            <span style={{ viewTransitionName: "n" }}>
+                                ✏️ 공부 기록
+                            </span>
+                        </Link>
+                    </div>
+                )}
+            </div>
+        );
+    };
+
+    const renderMainTitle = () => {
+        if (!isHome) {
+            return isN ? (
+                <h1 className="text-3xl sm:text-4xl font-bold">
+                    <span style={{ viewTransitionName: "n" }}>
+                        ✏️ 공부 기록
+                    </span>
+                </h1>
+            ) : (
+                <h1 className="text-3xl sm:text-4xl font-bold">{title}</h1>
+            );
+        }
+        return null;
+    };
 
     return (
         <header
@@ -27,34 +78,11 @@ const Header: React.FC<HeaderProps> = ({
         >
             <div>
                 {isHome && <Quote />}
-                <h1 className="text-3xl sm:text-4xl font-bold">
-                    {isHome ? (
-                        <span style={{ viewTransitionName: "name" }}>
-                            {MainTitle}
-                        </span>
-                    ) : (
-                        <div className="flex items-center space-x-1">
-                            <Link href="/">
-                                <ArrowLeft className="size-3 sm:size-4" />
-                            </Link>
-                            <Link
-                                href="/"
-                                className="text-sm sm:text-base font-semibold text-secondary-foreground hover:underline"
-                            >
-                                <span style={{ viewTransitionName: "name" }}>
-                                    {MainTitle}
-                                </span>
-                            </Link>
-                        </div>
-                    )}
+                <h1 className="text-3xl sm:text-4xl font-bold mb-2">
+                    {renderSubTitle()}
                 </h1>
-                {!isHome && (
-                    <h1 className="text-3xl sm:text-4xl font-bold mt-2">
-                        {title}
-                    </h1>
-                )}
+                {renderMainTitle()}
             </div>
-
             <div style={{ viewTransitionName: "mode" }}>
                 <ModeToggle />
             </div>
