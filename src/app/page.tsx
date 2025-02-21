305d67857bb01d560e88a48e27915a8eade74c24
Dohyeop Lim <dhlim0817@gmail.com>
2025-02-22 03:29:55 +0900

    feat(Home): add link to /n

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,25 @@
 
 import Header from "@/components/Header";
 import PreviewList from "@/components/Blog/PreviewList";
+import { Link } from "next-view-transitions";
+import { ArrowRight } from "lucide-react";
 
 const Home = () => {
     return (
         <>
             <Header />
+
+            <Link href="/n">
+                <div className="w-full p-4 mb-10 rounded-md bg-gray-100 dark:bg-gray-800 hover:bg-gray-200 dark:hover:bg-gray-700 transition-colors duration-200">
+                    <h2 className="text-lg font-semibold">✏️ 공부 기록</h2>
+                    <div className="w-full flex items-center justify-between">
+                        <p className="text-sm text-secondary-foreground mt-2">
+                            재밌는 거도 많아요
+                        </p>
+                        <ArrowRight size={16} />
+                    </div>
+                </div>
+            </Link>
 
             <PreviewList />
         </>
