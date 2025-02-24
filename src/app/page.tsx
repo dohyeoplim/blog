71122e42bd6622bfe98e14910b316deddd6338ea
Dohyeop Lim <dhlim0817@gmail.com>
2025-02-25 02:45:26 +0900

    feat(Home): restructure layout and add About Me link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,17 +10,35 @@ const Home = () => {
         <>
             <Header />
 
-            <Link href="/n">
-                <div className="w-full p-4 mb-10 rounded-md bg-gray-100 dark:bg-gray-800 hover:bg-gray-200 dark:hover:bg-gray-700 transition-colors duration-200">
-                    <h2 className="text-lg font-semibold">✏️ 공부 기록</h2>
-                    <div className="w-full flex items-center justify-between">
-                        <p className="text-sm text-secondary-foreground mt-2">
-                            재밌는 것도 많아요
-                        </p>
-                        <ArrowRight size={16} />
+            <div className="grid grid-cols-1 gap-4 md:grid-cols-2 mb-8">
+                <Link href="/about">
+                    <div className="w-full p-4 rounded-md bg-gray-100 dark:bg-gray-800 hover:bg-gray-200 dark:hover:bg-gray-700 transition-colors duration-200">
+                        <h2 className="text-lg font-semibold">👋 About Me</h2>
+                        <div className="w-full flex items-center justify-between">
+                            <p className="text-sm text-secondary-foreground mt-2">
+                                제가 누군나면요..
+                            </p>
+                            <ArrowRight size={16} />
+                        </div>
                     </div>
-                </div>
-            </Link>
+                </Link>
+
+                <Link href="/n">
+                    <div className="w-full p-4 rounded-md bg-gray-100 dark:bg-gray-800 hover:bg-gray-200 dark:hover:bg-gray-700 transition-colors duration-200">
+                        <h2 className="text-lg font-semibold">
+                            ✏️ 공부 기록 보기
+                        </h2>
+                        <div className="w-full flex items-center justify-between">
+                            <p className="text-sm text-secondary-foreground mt-2">
+                                재밌는 것도 많아요
+                            </p>
+                            <ArrowRight size={16} />
+                        </div>
+                    </div>
+                </Link>
+            </div>
+
+            <h2 className="text-lg font-semibold mb-4 pl-4">최근 블로그 글</h2>
 
             <PreviewList />
         </>
