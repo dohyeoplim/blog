f9d6775e79ced10b7ca77b1a76c0b9fd44c86445
Dohyeop Lim <dhlim0817@gmail.com>
2025-02-25 15:13:38 +0900

    feat(PostMetaInfo): add containerClassName prop for customizable container styling

--- src/components/Common/PostMetaInfo.tsx
+++ src/components/Common/PostMetaInfo.tsx
@@ -1,19 +1,25 @@
 import Time from "../Time";
 import TagList from "./TagList";
 
-interface PostMetaInfoProps {
+export interface PostMetaInfoProps {
     date: string;
     tags?: string[];
     badgeClassName?: string;
+    containerClassName?: string;
 }
 
 const PostMetaInfo: React.FC<PostMetaInfoProps> = ({
     date,
     tags,
     badgeClassName,
+    containerClassName,
 }) => {
     return (
-        <div className="flex items-center justify-start space-x-2 sm:space-x-3 mt-4 mb-8">
+        <div
+            className={`flex items-center justify-start space-x-2 sm:space-x-3 ${
+                containerClassName ?? "mt-4 mb-8"
+            }`}
+        >
             <Time date={date} />
             {tags && tags.length > 0 && (
                 <TagList tags={tags} badgeClassName={badgeClassName} />
